2504aeb17d9e08572bbd810da6d9baebc5a3da65
anasmohammed361 <anasmohammed361@gmail.com>
2022-10-04 08:40:53 +0530

    Altered check.py

--- india_map_game/main.py
+++ india_map_game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 import time
+from pyautogui import sleep
 from states import State
 screen=Screen()
 state=State()
@@ -7,7 +8,7 @@ screen.title("Inida Map Game")
 screen.bgpic("india.gif")
 screen.setup(width=600,height=800)
 while state.found<=28:
-    value=screen.textinput(f"{self.found}/29 States found","Enter a state :").title()
+    value=screen.textinput(f"{state.found}/29 States found","Enter a state :").title()
     print(value)
     state.check_state(value)
 state.complete()
